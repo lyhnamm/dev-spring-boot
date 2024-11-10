0f5be51efa00e17c85a3834e0889654fdb679c4e
Nam Hai Ly <157506336+lyhnamm@users.noreply.github.com>
2024-11-10 13:59:41 +0700

    Merge branch 'dev' into 'master': "Convert to use Lombok to auto-generate boilerplate code" (#10)
    * Convert to use Lombok to auto-generate boilerplate code

---
+++ 04-spring-boot-crud-demo/03-spring-boot-crud-employee-lombok/src/test/java/com/hainam/srpingboot/employee/EmployeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hainam.srpingboot.employee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
