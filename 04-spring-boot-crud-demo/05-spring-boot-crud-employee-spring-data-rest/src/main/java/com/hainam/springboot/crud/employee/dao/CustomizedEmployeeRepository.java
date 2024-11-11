abde50b44df8562ccf18c710ea215b6036eacf47
Nam Hai Ly <157506336+lyhnamm@users.noreply.github.com>
2024-11-12 00:31:10 +0700

    Add customize endpoint with Spring Data REST (#15)

--- 04-spring-boot-crud-demo/05-spring-boot-crud-employee-spring-data-rest/src/main/java/com/hainam/springboot/crud/employee/dao/CustomizedEmployeeRepository.java
+++ 04-spring-boot-crud-demo/05-spring-boot-crud-employee-spring-data-rest/src/main/java/com/hainam/springboot/crud/employee/dao/CustomizedEmployeeRepository.java
@@ -1,4 +1,13 @@
 package com.hainam.springboot.crud.employee.dao;
 
+import com.hainam.springboot.crud.employee.model.Employee;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
 public interface CustomizedEmployeeRepository {
+
+    @Query(" from Employee where firstName like %:name% or lastName like %:name% ")
+    List<Employee> findByName(@Param("name") String name);
 }
